977f6681c44fa814840299efeb7536251b746008
Athulsib <106370011+athulsib@users.noreply.github.com>
2025-12-16 15:10:26 +0530

    remove empty processor registration line

--- src/main/java/me/athulsib/stomcheat/StomCheat.java
+++ src/main/java/me/athulsib/stomcheat/StomCheat.java
@@ -54,8 +54,6 @@ public class StomCheat {
             //Load the checks separate from the player to make it more accessible
             this.checkManager.registerDefaultChecks();
 
-            instance.getProcessorManager().registerProcessorsFromPackage();
-
             //TODO Add more commands
             // Register commands
             MinecraftServer.getCommandManager().register(new ACInfoCommand());
